b5f54559faf9ce9e4876fafadc10049b4ad1381d
Madura Herath <madurah@conceptvines.com>
2023-03-03 15:09:14 +0530

    adding sample Ajax script

---
+++ SampleAjax.js
@@ -0,0 +1,45 @@
+import { AjaxModuleSchema } from 'journey-app';
+
+
+export const AjaxModule: AjaxModuleSchema = {
+
+   post: {
+
+       getUserName: (param:GetParams, config:ServiceConfig) => {
+
+           setSubPath(config);
+
+           return getUserName(param, config)
+
+       }
+
+   }
+
+ }
+
+
+ function getUserName(name, config) {
+    const url = "https://5250-112-135-222-22.ap.ngrok.io/greeting";
+    const payload = JSON.stringify({ name: name });
+  
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: payload
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        return data;
+      })
+      .catch(error => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  }
+  
